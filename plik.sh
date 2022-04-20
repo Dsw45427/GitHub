264552c1c45877d98a90017e307e830a13886e02
Dsw45427 <104094472+Dsw45427@users.noreply.github.com>
2022-04-20 19:37:59 +0200

    Add files via upload

---
+++ plik.sh
@@ -0,0 +1,14 @@
+git commit
+git checkout -b <nazwa gałęzi>
+git merge
+git branch
+git rebase
+git checkout C3
+git checkout main^
+git checkout HEAD~4
+git branch -f
+git reset <>~1
+git revert
+git tag v0 C1
+git cherry-pick C2
+
